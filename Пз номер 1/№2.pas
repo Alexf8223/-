81d277fa6f62682007cf8d7c1bc621b8c51b4ae2
Alexf8223 <98283329+Alexf8223@users.noreply.github.com>
2022-02-24 01:14:57 +0300

    Add files via upload

---
+++ Пз номер 1/№2.pas
@@ -0,0 +1,9 @@
+﻿// Составить программу вычесления Y=2(x)^2-3*(x^2+1)^2.
+program vichesleniya;
+var 
+  x:=readreal('Введите х');
+  Y:real;
+  begin 
+    Y:=2*sqr(x)-3*sqr(sqr(x)+1);
+    print ('Y = ',Y);
+  end.
